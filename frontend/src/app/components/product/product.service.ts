75d24df8153503a4eb461b11107f8a742c5ee980
Matheus Carvalho <matheuscarva@hotmail.com>
2020-05-26 10:44:37 -0300

    added update method to service

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -19,6 +19,16 @@ export class ProductService {
   read(): Observable<Product[]> {
     return this.http.get<Product[]>(this.baseUrl)
   }
+
+  readById(id: string): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`
+    return this.http.get<Product>(url)
+  }
+
+  update(product: Product): Observable<Product> {
+    const url = `${this.baseUrl}/${product.id}`
+    return this.http.put<Product>(url, product)
+  }
   
   showMessage(msg: string): void {
     this.snackBar.open(msg, 'Close', {
