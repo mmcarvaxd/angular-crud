fa6cb81df1e752bbe2de51e3daf05a48c915bb02
Matheus Carvalho <matheuscarva@hotmail.com>
2020-05-26 12:18:29 -0300

    added delete function

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -28,7 +28,8 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import localePt from '@angular/common/locales/pt'
 import {registerLocaleData} from '@angular/common';
-import { ProductUpdateComponent } from './components/product/product-update/product-update.component'
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component'
 
 registerLocaleData(localePt)
 
@@ -42,7 +43,8 @@ registerLocaleData(localePt)
     ProductCrudComponent,
     ProductCreateComponent,
     ProductReadComponent,
-    ProductUpdateComponent
+    ProductUpdateComponent,
+    ProductDeleteComponent
   ],
   imports: [
     BrowserModule,
--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -29,6 +29,11 @@ export class ProductService {
     const url = `${this.baseUrl}/${product.id}`
     return this.http.put<Product>(url, product)
   }
+
+  delete(id): Observable<Product>{
+    const url = `${this.baseUrl}/${id}`
+    return this.http.delete<Product>(url)
+  }
   
   showMessage(msg: string): void {
     this.snackBar.open(msg, 'Close', {
